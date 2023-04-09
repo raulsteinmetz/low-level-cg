748f577e7e8ae135d084ed49f982c28f1086bc08
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-04-09 11:28:06 -0300

    more cleaning

--- gl_1_canvasGlut/src/app_manager.cpp
+++
@@ -1,9 +0,0 @@
-#include "app_manager.h"
-
-
-AppManager::AppManager() {
-    this->app_state = MENU;
-    this->current_function = FUNCTION_NONE;
-    this->screen_height = SCREENHEIGHT;
-    this->screen_width = SCREENWIDTH;
-}
